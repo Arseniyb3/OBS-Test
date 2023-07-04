383acb2ba6627854a7d9515d6ad8130ec1dfb91e
Arseniyb3 <arseniyn35@gmail.com>
2023-07-05 01:18:45 +0300

    Final

--- app/models.py
+++ app/models.py
@@ -1,3 +1,3 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User, Group, Permission
+from django.contrib.contenttypes.models import ContentType
